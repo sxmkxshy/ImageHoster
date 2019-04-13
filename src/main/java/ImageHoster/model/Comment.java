d612fe89d8173d36b4e6c8c944f7bf00532f7bec
sxmkxshy <sambudhskr@outlook.com>
2019-04-12 22:05:10 -0700

    merge bug fixes with new core functionality

--- src/main/java/ImageHoster/model/Comment.java
+++ src/main/java/ImageHoster/model/Comment.java
@@ -1,4 +1,54 @@
 package ImageHoster.model;
 
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+//@Entity annotation specifies that the corresponding class is a JPA entity
+@Entity
+//@Table annotation provides more options to customize the mapping.
+//Here the name of the table to be created in the database is explicitly mentioned as 'Comment'.
+// Hence the table named 'Comment' will be created in the database with all the columns mapped to all the attributes in 'Tag' class
+@Table(name = "Comment")
 public class Comment {
+
+    //@Id annotation specifies that the corresponding attribute is a primary key
+    @Id
+    //@Column annotation specifies that the attribute will be mapped to the column in the database.
+    //Here the column name is explicitly mentioned as 'id'
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "text")
+    private String text;
+
+    @Column(name = "date")
+    private Date date;
+
+    //The 'comment' table is mapped to 'users' table with Many:One mapping
+    //One comment can have only one user (owner) but one user can have multiple comments
+    //FetchType is EAGER
+    @ManyToOne(fetch = FetchType.EAGER)
+    //Below annotation indicates that the name of the column in 'comment' table referring the primary key in 'users' table will be 'user_id'
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+    //The 'comment' table is mapped to 'images' table with Many:One mapping
+    //One comment can only be linked to one image but one image can have multiple comments
+    //FetchType is EAGER
+    @ManyToOne(fetch = FetchType.EAGER)
+    //Below annotation indicates that the name of the column in 'comment' table referring the primary key in 'users' table will be 'user_id'
+    @JoinColumn(name = "images_id")
+    private Image image;
+
+
+    public Comment() {
+    }
+
+    public Comment(String commentText) {
+        this.text = commentText;
+    }
 }
+
